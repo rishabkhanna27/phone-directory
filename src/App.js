b26daa5c0e10d7609f4ecf399c238b2f9af41f55
rishab1999 <rishabkhanna27@gmail.com>
2019-12-04 11:49:07 +0530

    Styling: Styled 'App' (Home Page)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './Header.js';
+import './App.css';
 function App() {
   return (
- <div>
-  <Header/>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
+  <div>
+    <Header />
+    <div className="component-body-container">
+      <button className="custom-btn add-btn">Add</button>
+      <div className="grid-container heading-container">
+        <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
         </div>
  </div>
   );
